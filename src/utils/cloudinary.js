f27b51a689241ddad61fa146b93190740c5eccf1
Aliyaj Ahemad <aliyaj.ahemad@appstrail.com>
2023-12-24 00:30:40 +0530

    file uploading half done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,10 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'doqiqqcok', 
+  api_key: '119759432279321', 
+  api_secret: 'juE5luS4dksCVnqdjjRRiydvsoM' 
+});
+
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
